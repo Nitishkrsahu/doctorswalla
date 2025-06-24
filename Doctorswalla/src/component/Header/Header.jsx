62af263b69900199b48fedfff6316b62fbf59cbe
Nitish <nitishkrsahu123@gmail.com>
2025-06-24 18:53:08 +0530

    header responsive

--- Doctorswalla/src/component/Header/Header.jsx
+++ Doctorswalla/src/component/Header/Header.jsx
@@ -1,38 +1,52 @@
-import './header.css'
-import {Link} from 'react-router-dom'
-export function Header(props){
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+export function Header(props) {
     return (
+
         <>
-        <header style={{
-            top: '-25px',
-            position: "sticky",
-            zIndex: "9999",
-            transition: "0.4s"
-        }}
-            >
-            <div className='text-white row'>
-                <div className='col-8'>
-                    <span className='bi bi-geo-alt-fill text-success pe-2'></span>
-                    <span>{props.location}</span>
+            <header>
+                <div className='d-flex justify-content-around bg-dark'>
+                    <div className='ps-lg-5'>
+                        <span className='bi bi-geo-alt-fill text-success'> 90, Khaitan Nagar Rd, Jinkal</span>
+                    </div>
+                    <div className='navbar navbar-dark bg-dark navbar-expand-lg ps-lg-5'>
+                        <div className='navbar-collapse collapse ps-lg-5' id="btn">
+                            <ul className='navbar-nav ps-lg-5'>
+                                <li className='nav-item'><span className='text-success'></span></li>
+                                <li className='nav-item'><span className='bi bi-telephone-fill text-white'> <span >+918651295487</span></span></li>
+                                <li className='nav-item ps-4'>
+                                    <select className='form-select form-select-sm bg-dark text-white'>
+                                        <option selected>English</option>
+                                        <option value={1}>Hindi</option>
+                                    </select>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className='col-4 text-end'>
-                    <span className='bi bi-telephone-fill pe-2'></span>
-                    <span>{props.tel}</span>
-                </div>
-            </div>
-            <div className='d-flex justify-content-between align-items-center text-white fs-4' style={{
-                height: '60px'
-            }}>
-                <div className='text-white'>
-                    <h3>{props.brand}</h3>
-                </div>
-                <div>
-                    <button className='btn btn-dark bi bi-truck me-3'></button>
-                    <button className='btn btn-dark bi bi-cart4 me-3'></button>
-                    <button className='btn btn-success px-4'><span className='bi bi-lock-fill pe-2'>SignIn</span></button>
-                </div>
-            </div>
-        </header>
+                <nav className='navbar navbar-dark bg-dark navbar-expand-lg ps-lg-5'>
+                    <div className='container-fluid ps-lg-5'>
+                        <Link to="/" className='navbar-brand pe-lg-5 text-success fw-bold'>Doctorswalla</Link>
+                        <button className='navbar-toggler' data-bs-target="#menu" data-bs-toggle="collapse">
+                            <span className='navbar-toggler-icon'></span>
+                        </button>
+                        <div className='navbar-collapse collapse ps-lg-5' id='menu'>
+                            <ul className='navbar-nav pe-lg-5'>
+                                <li className='navbar-item'><Link to="/" className='nav-link'><button className='btn btn-dark nav-item-text text-white'>Home</button></Link></li>
+                                <li className='navbar-item'><Link to="/medicine" className='nav-link'><button className='btn btn-dark nav-item-text text-white'>Categories</button></Link></li>
+                                <li className='navbar-item'><Link to="/lab" className='nav-link'><button className='btn btn-dark nav-item-text text-white'>Stores</button></Link></li>
+                            </ul>
+                        </div>
+                        <div className='navbar-collapse collapse ps-lg-5' id="btn">
+                            <ul className='navbar-nav ps-lg-5'>
+                                <li className='navbar-item'><Link to="/"><button className='nav-item-text bi bi-truck btn btn-dark'></button></Link></li>
+                                <li className='navbar-item'><Link to="/"><button className='nav-item-text bi bi-cart4 btn btn-dark'></button></Link></li>
+                                <li className='navbar-item'><Link to="/"><button className='nav-item-text bi bi-lock-fill btn btn-success'><span className='px-3'>SigIn</span></button></Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
         </>
     )
 }
